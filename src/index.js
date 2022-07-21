8c5b531b2dad3db13cd84e86f77e986276783bf2
John J Setlock II <johnsetlock@gmail.com>
2022-07-21 08:02:40 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,77 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+
+  // get the reuired elements from the DOM
+
+  const taskForm= document.querySelector('#create-task-form');
+  const newTask= document.querySelector('#new-task-description');
+  const taskList= document.getElementById('tasks');
+
+
+  // add dropdown list  **not finished yet
+  // const dropDown=document.createElement('select');
+  // dropDown.style.color='red';
+  
+  // const optionGreen= document.createElement('option');
+  // optionGreen.value='green';
+  // optionGreen.style.color='green';
+  // optionGreen.text="Low Priority";
+
+  // const optionRed= document.createElement('option');
+  // optionRed.value='red';
+  // optionRed.style.color='red';
+  // optionRed.text="High Priority";
+
+  
+  // dropDown.append(optionRed);
+  // dropDown.append(optionGreen);
+  // taskForm.append(dropDown);
+  
+
+
+  
+
+
+  
+  //Event Listeners
+  
+  taskForm.addEventListener('submit',(e)=>{       // Create New Task
+    e.preventDefault();                           // stops the automatic refresh
+
+
+    // Create a new list element for the new task
+    const li =document.createElement('li');       
+    li.textContent=newTask.value;    
+    //li.style.color='blue';
+
+    
+    // Create a delete button for the new task
+    const btn=document.createElement('button');
+    btn.textContent='X';
+    li.append(btn);
+    taskList.append(li);
+
+    //  Handle the delete button
+    btn.addEventListener('click',(e)=>{
+      li.remove();
+    })
+   
+
+ 
+
+
+
+
+
+  });
+ 
+
+
+
+
+
+  
 });
+
+
